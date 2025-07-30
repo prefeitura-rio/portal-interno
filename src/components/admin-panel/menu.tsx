6971da75cb5bee8250cffa04689106e3ad54738b
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2025-07-30 11:31:21 -0300

    ref: keep it dry

--- src/components/admin-panel/menu.tsx
+++ src/components/admin-panel/menu.tsx
@@ -3,7 +3,6 @@
 import { Ellipsis, LogOut } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { useState } from 'react'
 
 import { CollapseMenuButton } from '@/components/admin-panel/collapse-menu-button'
 import { Button } from '@/components/ui/button'
@@ -14,6 +13,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '@/components/ui/tooltip'
+import { useLogout } from '@/hooks/use-logout'
 import { getMenuList } from '@/lib/menu-list'
 import { cn } from '@/lib/utils'
 
@@ -24,36 +24,7 @@ interface MenuProps {
 export function Menu({ isOpen }: MenuProps) {
   const pathname = usePathname()
   const menuList = getMenuList(pathname)
-  const [isLoading, setIsLoading] = useState(false)
-
-  const handleLogout = async () => {
-    if (isLoading) return // Prevent multiple clicks
-
-    setIsLoading(true)
-    try {
-      // uri de redirecionamento do keycloak
-      const redirectUri = `${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_BASE_URL}/auth?client_id=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_IDENTIDADE_CARIOCA_REDIRECT_URI}&response_type=code`
-      // primeiro faz logout do keycloak
-      await fetch('/api/auth/logout')
-      // depois faz logout do gov.br via iframe oculto
-      const govbrLogoutUrl = `${process.env.NEXT_PUBLIC_GOVBR_BASE_URL}logout?post_logout_redirect_uri=${encodeURIComponent(redirectUri)}`
-      const iframe = document.createElement('iframe')
-      iframe.style.display = 'none'
-      iframe.src = govbrLogoutUrl
-      document.body.appendChild(iframe)
-      //remover o iframe após o carregamento
-      iframe.onload = () => {
-        setTimeout(() => {
-          document.body.removeChild(iframe)
-          // Redireciona o usuário após o logout do govbr
-          window.location.href = redirectUri
-        }, 0) // coloca em ultima prioridade na stack de execução
-      }
-    } catch (error) {
-      console.error('Logout failed:', error)
-      setIsLoading(false) // Reset loading state on error
-    }
-  }
+  const { isLoading, handleLogout } = useLogout()
 
   return (
     <ScrollArea className="[&>div>div[style]]:!block">
