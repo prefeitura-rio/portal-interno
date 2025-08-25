8ac0c822cb9ecfa7f027fb3ae8b9293b9a42a993
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2025-08-25 17:24:42 -0300

    chore: http clients

--- orval.config.ts
+++ orval.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'orval'
 export default defineConfig({
   api: {
     input:
-      'https://github.com/prefeitura-rio/app-go-api/blob/main/docs/openapi-v3.json',
+      'https://raw.githubusercontent.com/prefeitura-rio/app-go-api/refs/heads/staging/docs/openapi-v3.json',
     output: {
       target: './src/http/api.ts',
       schemas: './src/http/models',
